f43593a264f571bf449f1fa162affb5bbe4965c7
Lekar Bogdan <bogdan.fraer@mail.ru>
2017-10-04 11:23:06 +0300

    modified text - add html tegs and connect css

--- index.php
+++ index.php
@@ -11,6 +11,30 @@ $m = strftime('%m', $date);
 $y = strftime('%Y', $date);
 $count_y = $y - $birthday;
 
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>My Kitty Birthday</title>
+
+<meta name="description" content=""> 
+
+<script type="text/javascript" src="scripts/jquery-3.1.1.min.js"></script>
+
+<!--[if lt IE 8]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<link href="_styles/IE.css" rel="stylesheet" type="text/css" />
+<![endif]-->
+
+<link href="main.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<div id="birthdey_program">
+<?php
 echo "<p>Малышка родилась - {$dey_birthday}.{$birthday}</p>";
 if ($day == $dey_birthday) {
     echo "<p>Сегодне Леточке День Рождения! Ей - {$count_y}!</p>";
@@ -19,6 +43,8 @@ if ($day == $dey_birthday) {
 } else {
     echo "<p> В этом году Леточке уже был день рождения. Ей - {$count_y}!</p>";
 }
-
-
 ?>
+</div>
+
+</body>
+</html> 
